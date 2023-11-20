d0d56b5a6bfc3364e008a37bbe95ce23b02c179a
costin monica <costinmonica.98@gmail.com>
2023-11-20 14:48:48 +0200

    Am adaugat un calendar in sectiune paginii principale.

--- script.js
+++ script.js
@@ -12,5 +12,20 @@ else {
 }
 
 }
+// calendar// 
+let container = document.querySelector(".container");
+let toggleBtn = document.querySelector('.toggle-btn');
 
+dycalendar.draw({
+   target:'#dycalendar', 
+   dayformat: 'full', 
+   type: 'month', 
+   monthformat: 'full', 
+   highlighttoday: true, 
+   prevnextbutton: 'show'
+});  
 
+toggleBtn.onclick = () =>{
+ container.classList.toggle('dark');
+ document.body.classList.toggle('background-dark');
+};
